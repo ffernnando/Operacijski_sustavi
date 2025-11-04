b8692d6483e2dac12b42580c09c0708bb83e605d
ffernnando <ffernnando192@gmail.com>
2025-11-04 16:55:14 +0100

    Riješi vježbu 2-d

--- OS_vjezba_2/vjezba_2d/vjezba_2_d.cpp
+++ OS_vjezba_2/vjezba_2d/vjezba_2_d.cpp
@@ -10,62 +10,98 @@
 
 using namespace std;
 
-int Id;
-int *ZajednickaVarijabla;
+int N;
+int trazim_id, broj_id;
+int *TRAZIM, *BROJ;
 
-void Pisac(int i) {
-  *ZajednickaVarijabla = i;
-}
+void sloziSegment() {
+  trazim_id = shmget(IPC_PRIVATE, sizeof(int) * N, 0600);
+  broj_id = shmget(IPC_PRIVATE, sizeof(int) * N, 0600);
+  if (trazim_id == -1 || broj_id == -1) {
+    exit(1);
+  }
 
-void Citac() {
-  int i;
+  TRAZIM = (int*)shmat(trazim_id, NULL, 0);
+  BROJ = (int*)shmat(broj_id, NULL, 0);
 
-  do {
-    i = *ZajednickaVarijabla;
-    cout<<"Procitano "<<i<<endl;
-    sleep(1);
-  } while(i == 0);
-  cout<<"Procitano je: "<<i<<endl;
-}
+
+  for(int i = 0; i < N; i++) {
+    TRAZIM[i] = 0;
+    BROJ[i] = 0;
+  }
+  
+};
 
 void brisi(int sig) {
-  shmdt(ZajednickaVarijabla);
-  shmctl(Id, IPC_RMID, NULL);
-  exit(0);
-}
+  shmdt(TRAZIM);
+  shmctl(trazim_id, IPC_RMID, NULL);
 
-//dodati cleanup u slučaju interrutpta (ctr + c)
-void izlaz(int sig) {
-  cout<<"Baj baj!"<<endl;
+  shmdt(BROJ);
+  shmctl(broj_id, IPC_RMID, NULL);
   exit(0);
 };
 
 
+int maksimum() {
+  int max = BROJ[0];
+  for (int i = 1; i < N; i++) {
+    if (BROJ[i] > max) max = BROJ[i];
+  }
+  return max;
+};
 
-int main() {
-  Id = shmget(IPC_PRIVATE, sizeof(int) * 100, 0600);
-  if (Id == -1) exit(1);
+void ulazKO(int i) {
+  TRAZIM[i] = 1;
+  BROJ[i] = maksimum() + 1;
+  TRAZIM[i] = 0;
 
-  ZajednickaVarijabla = (int*) shmat(Id, NULL, 0);
-  *ZajednickaVarijabla = 0;
+  for(int j = 0; j < N; j++) {
+    while (TRAZIM[j] != 0) {
+      sleep(1);
+    }
+    while (BROJ[j] != 0 && (BROJ[j] < BROJ[i] || ((BROJ[j] == BROJ[i]) && (j < i)))) {
+      sleep(1);
+    }
+  }
+};
 
+void izlazKO(int i) {
+  BROJ[i] = 0;
+};
+
+void proc(int i) {
+  for (int k = 1; k <= 5; k++) {
+    ulazKO(i);
+    for (int m = 1; m <= 5; m++) {
+      cout<<"Proces: "<<i+1<<", K.O. br: "<<k<<" ("<<m<<"/5)"<<endl;
+    }
+    izlazKO(i);
+  }
+};
+
+int main(int argc, char* argv[]) {
   sigset(SIGINT, brisi);
 
-  if (fork() == 0) {
-    Citac();
-    exit(0);
-  }
-  if (fork() == 0) {
-    sleep(5);
-    Pisac(123);
-    exit(0);
-  }
-  wait(NULL);
-  wait(NULL);
-  brisi(0);
-  //zauzimanje zajedničke memorije
+  N = atoi(argv[1]);
+  sloziSegment();
 
-  
-  //oslobađanje zauzete zajedničke memorije
+  int i;
+  for(i = 0; i < N; i++) {
+    switch(fork()) {
+      case 0: {
+        proc(i);
+        exit(0);
+      }
+      case -1: {
+        cout<<"GRESKA!"<<endl;
+        break;
+      }
+    }
+  }
+
+  for (int j = 0; j < N; j++) {
+    wait(NULL);
+  }
+  brisi(0);
   return 0; 
 };
